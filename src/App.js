f13f5536efe4b61a9fef7d94400ca88117358424
Christopher Wenceslaus <topher.wenceslaus@gmail.com>
2018-10-13 02:15:56 +0530

    upd

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">Mokka</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
